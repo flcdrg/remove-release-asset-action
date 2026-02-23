31acb0b3265e389f24f15d00a0757413f2b2b1d2
David Gardiner <david@gardinerfamily.au>
2026-02-23 21:24:52 +1030

    Switch to using rollup instead of ncc

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-import { run } from './remove-release-asset';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { minimatch } from 'minimatch';
 
-run();
+export async function run(): Promise<void> {
+  try {
+    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN!);
+
+    const { owner, repo } = github.context.repo;
+
+    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    const releaseId = parseInt(core.getInput('release_id', { required: true }));
+    const assetName = core.getInput('asset_name', { required: true });
+
+    const { data: assets } = await octokit.rest.repos.listReleaseAssets({
+      owner,
+      repo,
+      release_id: releaseId
+    });
+
+    for (const asset of assets.filter(a => minimatch(a.name, assetName))) {
+      try {
+        await octokit.rest.repos.deleteReleaseAsset({
+          owner,
+          repo,
+          asset_id: asset.id
+        });
+      } catch (error) {
+        core.warning(`Caught ${error}`);
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message ?? error);
+    }
+  }
+}
